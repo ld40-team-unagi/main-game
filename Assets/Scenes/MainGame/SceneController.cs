ce8a5e7edd3a19db1f1563d31f2603a2325afa04
HoshiUmeko <hoshi.kakiko@gmail.com>
2017-12-02 21:44:34 +0900

    画面クリックでタイトルへ遷移　#15

--- Assets/Scenes/MainGame/SceneController.cs
+++ Assets/Scenes/MainGame/SceneController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class scene : MonoBehaviour {
+public class SceneController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
