83c975f8761850ffd597b932d235e9cf3063a387
HoshiUmeko <hoshi.kakiko@gmail.com>
2017-12-04 02:12:39 +0900

    プレイヤーと警察との接触処理を追加　#80

---
+++ Assets/Scenes/MainGame/Enemy/Police.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Police : MonoBehaviour {
+
+	public GameObject Player;
+
+	void OnCollisionEnter(Collision other){
+
+		if (other.gameObject.name == "Player") {
+			Player.GetComponent<Player> ().isDead = true;
+			Debug.Log (Player.GetComponent<Player> ().isDead);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
