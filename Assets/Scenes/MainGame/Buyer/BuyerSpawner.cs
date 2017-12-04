e3517537db0d78f870b8622700b3b86961bcd67c
tanitta <ttata.trit@gmail.com>
2017-12-04 17:38:33 +0900

    Enable to spawn buyyer.

---
+++ Assets/Scenes/MainGame/Buyer/BuyerSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyerSpawner : MonoBehaviour {
+	public GameObject spawnedPrefab;
+	public float rangeRadius;
+
+	GameObject spawned;
+
+	// Update is called once per frame
+	void Update () {
+		if (spawned == null) {
+			Spawn ();
+		}
+	}
+
+	void Spawn(){
+		spawned = Instantiate(spawnedPrefab, spawningPosition(), Quaternion.identity);
+		spawned.transform.eulerAngles = new Vector3 (0f, Random.Range (0,360f), 0f);
+	}
+
+	Vector3 spawningPosition(){
+		Vector3 direction = new Vector3 (
+			Random.Range (-1f, 1f),
+			Random.Range (-1f, 1f),
+			Random.Range (-1f, 1f)
+		).normalized;
+		Vector3 position = direction * Random.value * rangeRadius;
+		Vector3 flatted = Vector3.Scale (position, new Vector3 (1f, 0f, 1f));
+		return flatted + transform.position;
+	}
+}
---
+++ Assets/Scenes/MainGame/SpawnerSpiner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerSpiner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0f, 0.5f + Random.value * 0.5f, 0f));
+	}
+}
